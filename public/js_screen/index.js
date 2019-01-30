0909ddeab35167e5c87acf17b7abfe51a3ea3cd6
YukiTsuchiy <46016304+YukiTsuchiy@users.noreply.github.com>
2019-01-30 17:18:49 +0900

    Update index.js

--- public/js_screen/index.js
+++ public/js_screen/index.js
@@ -151,7 +151,7 @@ Field.prototype = {
   addCircle: function (circle) {
     this.circles.push(circle);
     this.checkNumber(circle.color);
-    console.log(circle.props.command[0]);
+    console.log(circle.command);
   },
   winner: function (score) {
     // const names = {
